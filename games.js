2da544dc0804294c30550887da0e8acb50b99286
Zadow67 <officialwarmthful@gmail.com>
2026-09-05 02:48:07 +0530

    Map tells distance now (in KM).

---
+++ games.js
@@ -0,0 +1,101 @@
+const locations = [
+    [25.272234,51.421210],
+    [40.668083,-73.978557],
+    [49.166786,-122.801385],
+    [55.761226,37.594540],
+    [0.552119,35.306452],
+    [-16.696927,-49.265616],
+    [-12.015509,-77.084556],
+    [-0.173452,-78.476651],
+    [9.935848,-84.097897],
+    [14.851587,-91.523256],
+    [19.451904,-99.185354],
+    [43.725187,20.682714],
+    [47.066622,15.431666],
+    [45.775045,3.100527],
+    [40.231208,-3.763879],
+    [34.790922,10.757626],
+    [6.668074,-1.635519],
+    [14.879058,-15.874708],
+    [0.316390,32.567749],
+    [-1.951056,30.070275],
+    [18.772023,98.998754],
+    [15.105354,105.861119],
+    [11.547146,104.903693],
+    [25.031626,121.530959],
+    [34.130724,134.517785],
+    [35.198413,129.085509],
+    [3.212393,101.646219],
+    [1.293809,103.837003],
+    [-5.462038,122.601999],
+    [-37.808428,144.948607],
+    [-46.398375,168.375881],
+    [53.197116,50.156933],
+    [50.451966,30.521440],
+    [46.624390,14.312158],
+    [38.001498,23.722331],
+    [48.811937,9.201428],
+    [53.336864,-6.272377],
+    [52.470919,-1.896327],
+    [38.653047,-121.539730],
+];
+
+
+let guessedLocation = 0;
+const streetView = document.getElementById('street-view');
+
+function getHaversineDistance(lat1, lon1, lat2, lon2) {
+    const R = 6371;
+    const rLat1 = (lat1 * Math.PI) / 180;
+    const rLat2 = (lat2 * Math.PI) / 180;
+    const dLat = ((lat2 - lat1) * Math.PI) / 180;
+    const dLon = ((lon2 - lon1) * Math.PI) / 180;
+
+    const a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+              Math.cos(rLat1) * Math.cos(rLat2) *
+              Math.sin(dLon / 2) * Math.sin(dLon / 2);
+              
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    return R * c;
+}
+
+function randomLocation(apikey) {
+    let location = locations[Math.floor(Math.random() * locations.length)]
+
+    console.log(location)
+    const url = `https://www.google.com/maps/embed/v1/streetview?key=${apikey}&location=${location[0]},${location[1]}`
+    streetView.setAttribute('src',url)
+    return location;
+}
+
+const currentLocation = randomLocation("AIzaSyC5671eu0WOtBBmFtrIjuTzgkhBsdF7Z3U");
+
+
+function getGuessDistance(location,guessedLocation) {
+    const distance = getHaversineDistance(location[0],location[1],guessedLocation[0],guessedLocation[1])
+    console.log(`Distance: ${distance.toFixed(2)} km`);
+}
+
+
+
+
+const mapElement = document.querySelector(".map-view");
+
+const map = L.map(mapElement);
+
+map.setView(currentLocation, 5);
+L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    attribution: "&copy; OpenStreetMap contributors"
+}).addTo(map);
+
+var popup = L.popup()
+    .setLatLng(currentLocation)
+    .setContent('<p>Hello world!<br />This is a made by Zadow.</p>')
+    .openOn(map);
+
+
+map.on("click", function(event) {
+    guessedLocation = [event.latlng.lat, event.latlng.lng];
+    getGuessDistance(currentLocation, guessedLocation);
+    L.marker(guessedLocation).addTo(map);
+});
